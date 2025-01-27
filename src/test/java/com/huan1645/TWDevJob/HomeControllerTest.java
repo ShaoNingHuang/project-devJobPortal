b1cc91ec9dc97f8aa1c880ff6c965e6a81c8afb2
ynnos0111 <snhuang2@illinois.edu>
2025-01-26 23:40:11 -0600

    landing page and artifact upload

---
+++ src/test/java/com/huan1645/TWDevJob/HomeControllerTest.java
@@ -0,0 +1,33 @@
+package com.huan1645.TWDevJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@TestPropertySource("/application-test.properties")
+@AutoConfigureMockMvc
+public class HomeControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void getHomepage() throws Exception {
+        MvcResult mvcresult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andReturn();
+        ModelAndView mav = mvcresult.getModelAndView();
+        ModelAndViewAssert.assertViewName(mav, "index");
+    }
+}
