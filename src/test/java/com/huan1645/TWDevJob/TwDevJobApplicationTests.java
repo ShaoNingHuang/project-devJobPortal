f60263bb610ffab83c6eea7b0f9f1f84f77e7c5e
ynnos0111 <snhuang2@illinois.edu>
2025-01-26 20:52:21 -0600

    Project Setup

---
+++ src/test/java/com/huan1645/TWDevJob/TwDevJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huan1645.TWDevJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwDevJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
