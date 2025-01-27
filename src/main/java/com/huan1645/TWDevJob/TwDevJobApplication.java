bb0a842829f0583a270feb8655b69f8d2856aede
ynnos0111 <snhuang2@illinois.edu>
2025-01-26 20:52:21 -0600

    Project Setup

---
+++ src/main/java/com/huan1645/TWDevJob/TwDevJobApplication.java
@@ -0,0 +1,13 @@
+package com.huan1645.TWDevJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwDevJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwDevJobApplication.class, args);
+	}
+
+}
