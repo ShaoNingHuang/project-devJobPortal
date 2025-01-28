89c35e2c35198dfcd5aabb45dd22b0a97161fcb4
ynnos0111 <snhuang2@illinois.edu>
2025-01-27 23:34:06 -0600

    cd pipeline with dev db

--- src/test/java/com/huan1645/TWDevJob/TwDevJobApplicationTests.java
+++ src/test/java/com/huan1645/TWDevJob/TwDevJobApplicationTests.java
@@ -2,8 +2,10 @@ package com.huan1645.TWDevJob;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/application-test.properties")
 class TwDevJobApplicationTests {
 
 	@Test
